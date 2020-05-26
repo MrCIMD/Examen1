31cfd444eb2bd138ef3c2047407e4447872ad074
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2020-05-25 20:21:26 -0500

    Data en seeder

--- app/Http/Controllers/WeaponsController.php
+++ app/Http/Controllers/WeaponsController.php
@@ -9,12 +9,11 @@ class WeaponsController extends Controller
 {
     public function GetWeapons(){
         $weapons = Weapon::All();
-        return 'Hola mundo!';
-        // return json_encode($weapons);
+        return json_encode($weapons);
     }
 
     public function GetWeapon($id){
         $weapon = Weapon::where('id','=',$id)->get();
-        return 'Hola mundo!';
+        return json_encode($weapon);
     }
 }
--- database/seeds/insert_weapons.php
+++ database/seeds/insert_weapons.php
@@ -12,7 +12,11 @@ class insert_weapons extends Seeder
     public function run()
     {
         DB::table('weapons')->insert([
-            ["name" => "", "image" => "", "precision" => "", "scope" => "", "hurt" => ""]
+            ["name" => "Outlaw", "image" => "outlaw.jpg", "precision" => 55, "scope" => 95, "hurt" => 83],
+            ["name" => "Locus", "image" => "Locus.jpg", "precision" => 55, "scope" => 95, "hurt" => 95],
+            ["name" => "DL Q33", "image" => "DL Q33.jpg", "precision" => 60, "scope" => 95, "hurt" => 90],
+            ["name" => "Artic.50", "image" => "artic50.jgp", "precision" => 52, "scope" => 95, "hurt" => 85],
+            ["name" => "M21 EBR", "image" => "M21.jpg", "precision" => 60, "scope" => 90, "hurt" => 80]
         ]);
     }
 }
