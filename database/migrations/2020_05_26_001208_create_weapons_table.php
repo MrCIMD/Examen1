e2d475310896086e7411dcc13f8ccb707172f914
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2020-05-25 19:30:02 -0500

    Create migration and seeder weapons (seeder no data)

---
+++ database/migrations/2020_05_26_001208_create_weapons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeaponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weapons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('image', 100);
+            $table->bigInteger('precision');
+            $table->bigInteger('scope');
+            $table->bigInteger('hurt');
+            $table->timestamps();
+            $table->softDeletes('deleted_at', 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weapons');
+    }
+}
